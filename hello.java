4d5bfa97414f159f1057a16b50476ee4f3076604
Shrikar-03 <148742469+Shrikar-03@users.noreply.github.com>
2024-11-21 22:22:12 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,25 +1,13 @@
-import java.util.Scanner; // Importing the Scanner class for input
-
 public class Hello {
     public static void main(String[] args) {
-        // Create a Scanner object for taking input
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user for the first number
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        // Prompt the user for the second number
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
+        // Declare two numbers
+        int num1 = 15;
+        int num2 = 25;
 
         // Perform addition
         int sum = num1 + num2;
 
         // Display the result
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-
-        // Close the scanner
-        scanner.close();
     }
 }
